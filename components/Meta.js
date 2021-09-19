ed0fa7b65af5d4a8f3c43c872ef2eefee0dc8c59
ed louise amor <ed.amor@xlog.com>
2021-09-19 21:01:00 +0800

    fix view in mobile browser

--- components/Meta.js
+++ components/Meta.js
@@ -6,7 +6,6 @@ export const Meta = ({ title }) => {
       <>
         <Head>
           <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
           <link rel="icon" href="/favicon-32.png" />
           <title>{`${title} | Staffstore Inc`}</title>
           <meta 
--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ const MyApp = ({ Component, pageProps }) => {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="icon" href="/favicon-32.png" />
         <meta 
           name="description" 
